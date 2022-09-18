236b2b4874b1801f87938f331c5ef7c3626b5823
JaQuan Banks <shadic151@gmail.com>
2022-09-18 18:35:11 -0400

    commit tree c6f32ee36318a62607d7100b32b6b2beea7db276

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+    const cats = ["Milo", "Otis", "Garfield"]; {
+
+    }
+
+   function destructivelyAppendCat() {
+    cats.push("Ralph")
+   }
+
+   function destructivelyPrependCat() {
+    cats.unshift("Bob")
+   }
+
+   function destructivelyRemoveLastCat() {
+    cats.pop()
+   }
+
+   function destructivelyRemoveFirstCat() {
+    cats.shift()
+   }
+    function appendCat() {
+    const appendCat = [...cats, "Broom"]; {
+        return appendCat
+    }
+    }
+
+    function prependCat() {
+        const prependCat = ["Arnold", ...cats]; {
+            return prependCat
+        }
+    }
+
+    function removeFirstCat() {
+        const removeFirstCat = cats.slice(1); {
+            return removeFirstCat
+        }
+    }
+
+    function removeLastCat() {
+        const removeLastCat = cats.slice(0, 2); {
+            return removeLastCat
+        }
+    }
+
